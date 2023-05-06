c5caf1368aa65a2bc06e6a23caeeff89279f46f5
MariusB <marius_beschieru@yahoo.com>
2023-05-06 18:56:50 +0300

    Responsive mobile menu

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -6,27 +6,82 @@ import useModalIsOpen from '../hooks/useModallsOpen';
 import useEmitters from '../hooks/useEmitters';
 import Home from '../modules/home';
 import SideNavbar from '../components/side-navbar';
-import { ColumnContainer, RowContainer } from './styles';
+import { ColumnContainer, HeaderLogoMobile, HeaderMobileMenu, InsideLink, RowContainer } from './styles';
 import TopNavBar from '../components/top-navbar';
+import { useAppDispatch, useAppSelector } from '../hooks/store-hooks';
+import { appWidthSelector, isModalOpenAppStateSelector } from '../store/selectors/app-selectors';
+import { setModalOpenAction } from '../store/slices/appSlice';
+import './styles.css';
+import headerLogo from './header_logo.png';
 
 const AppRouter = () => {
   useModalIsOpen();
   useEmitters();
 
+  const dispatch = useAppDispatch();
+  const width = useAppSelector(appWidthSelector);
+  const isOpen = useAppSelector(isModalOpenAppStateSelector);
+
+  const buildButton = (): JSX.Element => {
+    return (
+      <button style={{ zIndex: 50 }} type="button" onClick={() => dispatch(setModalOpenAction(!isOpen))} className={`${isOpen ? 'active' : ''} burger`}>
+        <div className={`${isOpen ? 'active' : ''} strip burger-strip-4`}>
+          <div />
+          <div />
+          <div />
+        </div>
+      </button>
+    );
+  };
+
+  if (width > 1000) {
+    return (
+      <BrowserRouter>
+        <AppLoaderOverlay />
+        <RowContainer>
+          <SideNavbar />
+          <ColumnContainer>
+            <TopNavBar />
+            <Routes>
+              <Route index element={<Home />} />
+              {/* <Route path="*" element={<NotFound />} /> */}
+            </Routes>
+          </ColumnContainer>
+        </RowContainer>
+      </BrowserRouter>
+    );
+  }
   return (
     <BrowserRouter>
-      <AppLoaderOverlay />
-
-      <RowContainer>
-        <SideNavbar />
-        <ColumnContainer>
-          <TopNavBar />
-          <Routes>
-            <Route index element={<Home />} />
-            {/* <Route path="*" element={<NotFound />} /> */}
-          </Routes>
-        </ColumnContainer>
-      </RowContainer>
+      <ColumnContainer>
+        {!isOpen && buildButton()}
+        <Routes>
+          <Route index element={<Home />} />
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </ColumnContainer>
+      <HeaderMobileMenu isOpen={isOpen}>
+        <div style={{ position: 'absolute', top: '20px', right: '30px' }}>{buildButton()}</div>
+        <HeaderLogoMobile isOpen={isOpen} src={headerLogo} />
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="dashboard">
+          Dashboard
+        </InsideLink>
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="appointment">
+          Appointment
+        </InsideLink>
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="doctors">
+          Doctors
+        </InsideLink>
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="patients">
+          Patients
+        </InsideLink>
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="chats">
+          Chats
+        </InsideLink>
+        <InsideLink isOpen={isOpen} onClick={() => dispatch(setModalOpenAction(false))} to="settings">
+          Settings
+        </InsideLink>
+      </HeaderMobileMenu>
     </BrowserRouter>
   );
 };
